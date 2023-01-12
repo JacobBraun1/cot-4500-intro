507fc33cfba969a1807d9abb07deec107aedaa25
Jacob Braun <jacobbraun86@gmail.com>
2023-01-12 16:02:54 -0500

    second

---
+++ src/main/intro_to_python.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+array = np.array([[0,0,0],[0,0,0],[0,0,0]])
+
+for x in range(0,3):
+  for y in range(0,3):
+    if x == y:
+      array[x][y] = 1
+    else:
+      array[x][y] = 0
+
+print(array)
+
+for x in range(0,3):
+  for y in range(0,3):
+    if x == y:
+      array[x][y] = 1
+    else:
+      array[x][y] = 3
+
+print(array)
+
+array = np.delete(array, 2, 1)
+
+print(array)
+
